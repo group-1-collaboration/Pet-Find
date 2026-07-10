43f85ddf03680eb85a0c2d5fcc672322fbd5eb2e
imaninaitore <imaninaitore1726@gmail.com>
2026-07-10 22:08:41 +0300

    feat: import about section

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import React from 'react'
 import PetList from './components/PetList'
 import Navbar from './components/Navbar'
 import Header from './components/Header'
+import AboutUs from './components/AboutUs'
 import Footer from './components/footer'
+
 
 function App() {
   return (
     <div>
       <Navbar />
       <Header />
+      <AboutUs />
       <PetList/>
       <Footer />
     </div>
