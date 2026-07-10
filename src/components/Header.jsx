5fd524307b5badef44dc4e33f379039c0a586f26
imaninaitore <imaninaitore1726@gmail.com>
2026-07-10 21:00:51 +0300

    feat: change background and add header statements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import backgroundImage from "@/assets/bgImage.jpg"
+import backgroundImage from "@/assets/headerbg.png"
 
 function Header() {
   return (
@@ -9,6 +9,9 @@ function Header() {
         backgroundPosition:"center",
         height: "500px",
         }}>
+          <h1 className='text-7xl text-white font-bold'>Everyone needs a </h1>
+          <span className='text-7xl text-orange-500 font-bold'>Home.</span>
+          <p className='text-xl text-white'>Open your heart. Change a life. <p>Find your perfect companion today.</p> </p>
     </header>
 
   )
