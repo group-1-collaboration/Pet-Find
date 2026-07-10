9447c2b628e1630f85452656a918bc3b0bd07aae
imaninaitore <imaninaitore1726@gmail.com>
2026-07-10 22:57:30 +0300

    feat: create link to login page using react router

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,27 @@ import Navbar from './components/Navbar'
 import Header from './components/Header'
 import AboutUs from './components/AboutUs'
 import Footer from './components/footer'
+import Login from './pages/Login'
 
-
-function App() {
-  return (
-    <div>
-      <Navbar />
+function Home(){
+  return(
+    <>
+     <Navbar />
       <Header />
       <AboutUs />
       <PetList/>
       <Footer />
-    </div>
+    </>
   )
 }
 
-export default App
+function App() {
+  return (
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/login" element={<Login/>} />
+      </Routes>
+  )
+}
+
+export default App;
