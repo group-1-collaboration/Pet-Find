263011bc276e7ce4d9a44e82059aa23036698eb1
imaninaitore <imaninaitore1726@gmail.com>
2026-07-10 21:05:40 +0300

    feat:move register button to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import backgroundImage from "@/assets/headerbg.png"
 
+
 function Header() {
   return (
     <header style={{
@@ -9,9 +10,23 @@ function Header() {
         backgroundPosition:"center",
         height: "500px",
         }}>
+          <div className='p-3'>
           <h1 className='text-7xl text-white font-bold'>Everyone needs a </h1>
           <span className='text-7xl text-orange-500 font-bold'>Home.</span>
+          </div>
+
+         <div className='p-3'>
           <p className='text-xl text-white'>Open your heart. Change a life. <p>Find your perfect companion today.</p> </p>
+         </div>
+
+        <div>
+          <Link to={'/register'}>
+             <Button className="bg-amber-500 hover:bg-amber-600 text-white text-primary-foreground px-3 py-1.5 rounded-md hover:opacity-90 transition-opacity">
+              sign up 
+             </Button>
+            </Link>
+        </div>
+            
     </header>
 
   )
