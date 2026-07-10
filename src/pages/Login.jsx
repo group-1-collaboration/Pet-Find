77e4a5d71d5c15de9255d778b65c6ab821af7ba1
imaninaitore <imaninaitore1726@gmail.com>
2026-07-10 22:57:47 +0300

    feat:import image for background

---
+++ src/pages/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import loginBackground from "@/assets/loginbg.png"
+
+function Login() {
+  return (
+    <section style={{
+            backgroundImage:`url(${loginBackground})`,
+            backgroundSize:"cover",
+            backgroundPosition:"center",
+            height: "500px",
+            }}>
+
+
+    </section>
+  )
+}
+
+export default Login
