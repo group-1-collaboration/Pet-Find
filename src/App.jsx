fdc5314c92ffa3a64d8ea73c2cd90ef7841f5c38
imaninaitore <imaninaitore1726@gmail.com>
2026-07-10 11:52:48 +0300

    feat:import header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import PetList from './components/PetList'
 import Navbar from './components/Navbar'
+import Header from './components/Header'
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Header />
       <PetList/>
     </div>
   )
