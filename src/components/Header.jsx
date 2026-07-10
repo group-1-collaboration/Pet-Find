86253b08bbed8a2f2eeedcdcbb72da709c358be5
imaninaitore <imaninaitore1726@gmail.com>
2026-07-10 21:39:28 +0300

    feat: add browse pets button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import backgroundImage from "@/assets/headerbg.png"
 import { Link } from 'react-router-dom'
 import { Button } from '@base-ui/react'
 import { PawPrint } from 'lucide-react'
+import { Search } from 'lucide-react'
 
 function Header() {
   return (
@@ -19,16 +20,26 @@ function Header() {
 
          <div className='p-3'>
           <p className='text-xl text-white'>Open your heart. Change a life. <p>Find your perfect companion today.</p></p>
-          <p className='text-xl text-orange-300'>Register, to adopt a pet or <a href="#login" className='text-orange-500 '> sign in </a> to existing account. </p>
+          <p className='text-lg text-orange-500'>Register, to adopt a pet or <a href="#login" className='text-orange-300 '> sign in </a> to your account. </p>
          </div>
 
-        <div className='p-4'>
-          <Link to={'/register'}>
+        <div className=' flex '>
+          <div className='p-3'>
+           <Link to={'/register'}>
              <Button className="flex items-center gap-2 bg-orange-500 hover:bg-amber-600 text-white text-primary-foreground px-3 py-1.5 rounded-md hover:opacity-90 transition-opacity">
                Register<PawPrint className='w-5 h-5'/> 
              </Button>
-            </Link>
+            </Link> 
+          </div>
+          <div className='p-3'>
+             <Button className="flex items-center gap-2 bg-slate-700 hover:bg-slate-600 text-white text-primary-foreground px-3 py-1.5 rounded-md hover:opacity-90 transition-opacity">
+               Browse pets<Search className='w-5 h-5'/> 
+             </Button>
+          </div>
+            
         </div>
+
+
             
     </header>
 
