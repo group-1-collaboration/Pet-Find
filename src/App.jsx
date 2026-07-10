158cd9cf37ffbf184b87240e1bfafb8b80a06a40
imaninaitore <imaninaitore1726@gmail.com>
2026-07-10 19:06:27 +0300

    feat: import footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PetList from './components/PetList'
 import Navbar from './components/Navbar'
 import Header from './components/Header'
+import Footer from './components/footer'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Header />
       <PetList/>
+      <Footer />
     </div>
   )
 }
