6f763fb7f9f2c6e6fd18c2c21bd98a615ab409ea
imaninaitore <imaninaitore1726@gmail.com>
2026-07-10 11:25:38 +0300

    feat:import navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import PetList from './components/PetList'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div>
+      <Navbar />
       <PetList/>
     </div>
   )
