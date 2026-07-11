678088f4dee268ff421d95056de8b944921aa032
imaninaitore <imaninaitore1726@gmail.com>
2026-07-11 12:54:22 +0300

    feat: create AuthContext with , login function.

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,36 @@
+import { createContext, useContext, useState } from 'react';
+
+const AuthContext = createContext(undefined); //global context to hold authentication data 
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(() => { //initializing the user
+    try {
+      const storedUser = localStorage.getItem('auth_user');
+      const storedToken = localStorage.getItem('auth_token');
+      
+      if (storedUser && storedToken) {
+        return JSON.parse(storedUser);
+      }
+    } catch (error) { //error handling
+      console.error("Failed to parse initialized auth session:", error);
+      localStorage.removeItem('auth_user');
+      localStorage.removeItem('auth_token');
+    }
+    return null;
+  });
+
+  const isAuthenticated = !!user;
+
+  // Login function 
+  const login = (token, userData) => {
+    localStorage.setItem('auth_token', token);
+    localStorage.setItem('auth_user', JSON.stringify(userData));
+    setUser(userData);
+  };
+
+  // Registration function
+  const register = (token, userData) => {
+    localStorage.setItem('auth_token', token);
+    localStorage.setItem('auth_user', JSON.stringify(userData));
+    setUser(userData);
+  };
