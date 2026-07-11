efdcef1fcbe4698ab7670e848d0957becd17de20
JeromeJason <jasonnem048@gmail.com>
2026-07-11 20:01:24 +0300

    fixbug: fix import routes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useMemo, useEffect, useCallback } from "react";
 import { PawPrint, LayoutGrid, PlusCircle, ClipboardList, Home, Loader2, Menu, X } from "lucide-react";
-import AddPet from "../component/AddPet";
-import EditPet from "../component/EditPet";
-import ManagePets from "../component/ManagePets";
-import ManageRequests from "../component/ManageRequests";
+import AddPet from "../components/AddPet";
+import EditPet from "../components/EditPet";
+import ManagePets from "../components/ManagePets";
+import ManageRequests from "../components/ManageRequests";
 
 // -----------------------------------------------------------------------------
 // Local JSON data source
