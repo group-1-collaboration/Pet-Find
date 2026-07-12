d0bc56c6680abbdc50d2ed1780e84afdcab4c940
Juan-ita <kavatajuanita@gmail.com>
2026-07-12 17:16:04 +0300

    feat: remove pet filtering

--- src/components/PetList.jsx
+++ src/components/PetList.jsx
@@ -1,3 +1,4 @@
+
 import {useState} from 'react'
 import PetCard from './PetCard';
 import SearchBar from './SearchBar';
@@ -13,20 +14,11 @@ function PetList() {
     const [search, setSearch] = useState("");
     // const [category, setCategory] = useState("All")
     const filteredPets = pets.filter((pet) => pet.Breed.toLowerCase().includes(search.toLowerCase()))
-
-    //stores the category when user clicked
-    const [selectedCategory, setSelectedCategory] = useState("all");
-
-    //filter pets
-    const CategoryPets = selectedCategory === "all" ?petData: petData.filter((pet)=> pet.category.toLowerCase()===selectedCategory.toLowerCase())
   return (
     <>
       <Categories setSelectedCategory={setSelectedCategory}/>
 
       <section className='max-w-6xl mx-auto px-6 py-10'>
-        <h2 className='text-3xp font-bold mb-8 text-center'>
-          {selectedCategory === "all" ?"All Pets": `${selectedCategory}s`}
-        </h2>
          {/* Display the search bar */}
         <SearchBar 
         search={search}
